5026fd377561f7d7ad8c76f5cdb3ca92425e136b
RolandBarro <rolandbarro@gmail.com>
2020-02-17 06:18:27 +0800

    initial scaffolding

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  // Google APIs
+  googleMapKey: 'AIzaSyDHqefQL_PHOnxs7JnR4YN41pTf24Ck9lg',
+  googleGeocodeUrl: 'https://maps.googleapis.com/maps/api/geocode/json',
 };
 
 /*
