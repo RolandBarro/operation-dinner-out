412373170dee766345da66aaf06778a214c36520
RolandBarro <rolandbarro@gmail.com>
2020-02-17 13:13:03 +0800

    implement typeahead search funcitonality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild, AfterViewInit } from '@angular/core';
 
 import { GoogleMappingService } from '../services/google-mapping.service';
 import { LocalDataService } from '../services/local-data.service';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Place } from '../models/place.model';
 
 @Component({
@@ -15,8 +15,15 @@ export class HomePage implements AfterViewInit {
   @ViewChild('mapContainer', { static: false }) mapContainer: any;
 
   places$: Observable<Place[]>;
+  private _foundPlaces$ = new BehaviorSubject<Place[]>([]);
+  foundPlaces$ = this._foundPlaces$.asObservable();
+
   places: Place[];
   selectedPlace: string;
+  searchText: string;
+  
+  hasSelected = false;
+  showResults = false;
 
   customPopoverOptions: any = {
     header: 'Select a place',
@@ -33,29 +40,55 @@ export class HomePage implements AfterViewInit {
     this.places$.subscribe((places: Place[]) => {
       if (places && places.length) {
         this.places = places;
-        this.selectedPlace = places[0].name;
-
-        if (this.selectedPlace) {
-          this.buildMap();
-        }
+        this.searchPlaces();
       }
     });
   }
 
   buildMap(): void {
-    this.googleAPIService.initializeMap(
-      this.mapContainer,
-      this.selectedPlace,
-      { zoom: 18, disableDefaultUI: true, tilt: 30 }
-    );
+    if (this.hasSelected) {
+      this.googleAPIService.initializeMap(
+        this.mapContainer,
+        this.selectedPlace,
+        { zoom: 16, disableDefaultUI: true, tilt: 30 }
+      );
+
+      this.hasSelected = false;
+    }
   }
 
-  locate(event: any) {
-    const selected = this.places.find(place => place.name === event.detail.value.name);
+  locate(place: Place) {
+    this.hasSelected = true;
+    this.searchText = place.name;
+    this.selectedPlace = place.name;
 
-    if (selected) {
-      this.selectedPlace = selected .name;
+    this.buildMap();
+    setTimeout(() => {
+      this.showResults = false;
+    }, 1000);
+  }
 
+  searchPlaces(event?: CustomEvent) {
+    let searchString = '';
+
+    if (event) {
+      searchString = event.detail.value.toLocaleLowerCase();
+    }
+
+    if (!this.selectedPlace && !this.searchText) {
+      this.selectedPlace = this.places[0].name;
+      this.hasSelected = true;
+      this._foundPlaces$.next(this.places);
+    } else {
+      this.showResults = true;
+      const foundPlaces: Place[] = this.places.filter((place: Place) => {
+        return place.name.toLocaleLowerCase().includes(searchString);
+      });
+
+      this._foundPlaces$.next(foundPlaces || null);
+    }
+
+    if (this.selectedPlace) {
       this.buildMap();
     }
   }
